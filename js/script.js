b2a98e46fb2a24bfc045613d4f770e1ad55ca3de
Spencer Pollock <srepollock@gmail.com>
2017-04-30 15:02:34 -0700

    :beer: Added some buttons for the slides

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 			$('#fullpage').fullpage({
         navigation: true,
-				navigationPosition: 'right',
+				// navigationPosition: 'right',
+                anchors: ['home', 'about', 'work', 'contact'],
+                menu: '#menu',
       });
 });
